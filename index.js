d8cbb780ba917a9b220492958707cc7e1008a219
Ramtin Rahvareh <ramtinravare1380@gmail.com>
2025-08-19 02:15:30 +0330

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+/*
+--> git installation commands <--
+git --version => show git current version;
+git help => show all git commands;
+git help "command name" => open a guide for this command;
+git config --global ... => setting some global configs;
+git config --list => show all current configs;
+
+--> git in projects <--
+touch "file name"."file format" (exam: index.js) => creating new file in curr directory;
+git init => first git initialization for project;
+git status => show project current changes and current status;
+git add "name file" => git will track changes in this file;
+git add . => git will track changes in all files;
+
+*/
+/*
+--> git conventions <--
+U file marker => untracked file
+A file marker => added file, a new file that begin to track by git
+M file marker => modified file 
+*/
