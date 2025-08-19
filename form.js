5613f960e9a2da552050dcf52eca59924831aaba
Ramtin Rahvareh <ramtinravare1380@gmail.com>
2025-08-20 01:16:22 +0330

    some changes for signup branch

--- form.js
+++ form.js
@@ -1,9 +1,10 @@
 console.log("hello there");
 console.log("hello there");
-console.log("hello there");
-console.log("hello there");
-console.log("hello there Ramtin");
+console.log("hello there John");
 
+console.log("please signup first");
+
+alert("you don't have an account yet");
 
 console.log("hello there");
 console.log("hello there");
